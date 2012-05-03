682bbf40272273a0158e0530eb6ebc3f8445e54d
Ron Yorston <rmy@pobox.com>
2012-05-03 19:49:21 +0100

    Cosmetic fixes; clarify that licence is GPLv2

--- sparsify.c
+++ sparsify.c
@@ -4,7 +4,7 @@
  * Copyright (C) 2004-2012 R M Yorston
  *
  * This file may be redistributed under the terms of the GNU General Public
- * License.
+ * License, version 2.
  */
 #include <ext2fs/ext2fs.h>
 #include <stdio.h>
@@ -14,35 +14,35 @@
 #define USAGE "usage: %s [-n] [-v] filesystem filename ...\n"
 
 struct process_data {
-	struct ext2_inode *inode ;
+	struct ext2_inode *inode;
 	unsigned char *buf;
 	int verbose;
-	int dryrun ;
+	int dryrun;
 	blk_t count;
 	blk_t blocks;
 	blk_t total_blocks;
 	int old_percent;
-} ;
+};
 
 static int process(ext2_filsys fs, blk_t *blocknr, e2_blkcnt_t blockcnt,
 					blk_t ref_block, int ref_offset, void *priv)
 {
-	struct process_data *p ;
-	errcode_t ret ;
-	int i, group ;
+	struct process_data *p;
+	errcode_t ret;
+	int i, group;
 
-	p = (struct process_data *)priv ;
+	p = (struct process_data *)priv;
 
 	p->blocks++;
 	if ( blockcnt >= 0 ) {
 		ret = io_channel_read_blk(fs->io, *blocknr, 1, p->buf);
 		if ( ret ) {
-			return BLOCK_ABORT ;
+			return BLOCK_ABORT;
 		}
 
 		for ( i=0; i < fs->blocksize; ++i ) {
 			if ( p->buf[i] ) {
-				break ;
+				break;
 			}
 		}
 
@@ -50,18 +50,18 @@ static int process(ext2_filsys fs, blk_t *blocknr, e2_blkcnt_t blockcnt,
 			p->count++;
 			
 			if ( !p->dryrun ) {
-				ext2fs_unmark_block_bitmap(fs->block_map, *blocknr) ;
+				ext2fs_unmark_block_bitmap(fs->block_map, *blocknr);
 				group = ext2fs_group_of_blk(fs, *blocknr);
 				fs->group_desc[group].bg_free_blocks_count++;
-				fs->super->s_free_blocks_count++ ;
+				fs->super->s_free_blocks_count++;
 				/* the inode counts blocks of 512 bytes */
-				p->inode->i_blocks  -= fs->blocksize / 512 ;
-				*blocknr = 0 ;
+				p->inode->i_blocks  -= fs->blocksize / 512;
+				*blocknr = 0;
 				/* direct blocks need to be zeroed in the inode */
 				if ( blockcnt < EXT2_NDIR_BLOCKS ) {
-					p->inode->i_block[blockcnt] = 0 ;
+					p->inode->i_block[blockcnt] = 0;
 				}
-				return BLOCK_CHANGED ;
+				return BLOCK_CHANGED;
 			}
 		}
 
@@ -77,105 +77,105 @@ static int process(ext2_filsys fs, blk_t *blocknr, e2_blkcnt_t blockcnt,
 		}
 	}
 
-	return 0 ;
+	return 0;
 }
 
 int main(int argc, char **argv)
 {
-	int verbose = 0 ;
-	int dryrun = 0 ;
-	errcode_t ret ;
-	int flags ;
-	int superblock = 0 ;
-	int open_flags = EXT2_FLAG_RW ;
+	int verbose = 0;
+	int dryrun = 0;
+	errcode_t ret;
+	int flags;
+	int superblock = 0;
+	int open_flags = EXT2_FLAG_RW;
 	int iter_flags = 0;
-	int blocksize = 0 ;
-	ext2_filsys current_fs = NULL;
-	struct ext2_inode inode ;
-	ext2_ino_t root, cwd, inum ;
-	int i, c ;
-	struct process_data pdata ;
+	int blocksize = 0;
+	ext2_filsys fs = NULL;
+	struct ext2_inode inode;
+	ext2_ino_t root, cwd, inum;
+	int i, c;
+	struct process_data pdata;
 
 	while ( (c=getopt(argc, argv, "nv")) != -1 ) {
 		switch (c) {
 		case 'n' :
-			dryrun = 1 ;
+			dryrun = 1;
 #if defined(BLOCK_FLAG_READ_ONLY)
 			iter_flags |= BLOCK_FLAG_READ_ONLY;
 #endif
-			break ;
+			break;
 		case 'v' :
-			verbose = 1 ;
-			break ;
+			verbose = 1;
+			break;
 		default :
-			fprintf(stderr, USAGE, argv[0]) ;
-			return 1 ;
+			fprintf(stderr, USAGE, argv[0]);
+			return 1;
 		}
 	}
 
 	if ( argc < optind+2 ) {
-		fprintf(stderr, USAGE, argv[0]) ;
-		return 1 ;
+		fprintf(stderr, USAGE, argv[0]);
+		return 1;
 	}
 
-	ret = ext2fs_check_if_mounted(argv[optind], &flags) ;
+	ret = ext2fs_check_if_mounted(argv[optind], &flags);
 	if ( ret ) {
 		fprintf(stderr, "%s: failed to determine filesystem mount state  %s\n",
-					argv[0], argv[optind]) ;
-		return 1 ;
+					argv[0], argv[optind]);
+		return 1;
 	}
 
 	if ( flags & EXT2_MF_MOUNTED ) {
 		fprintf(stderr, "%s: filesystem %s is mounted\n",
-					argv[0], argv[optind]) ;
-		return 1 ;
+					argv[0], argv[optind]);
+		return 1;
 	}
 
 	ret = ext2fs_open(argv[optind], open_flags, superblock, blocksize,
-							unix_io_manager, &current_fs);
+							unix_io_manager, &fs);
 	if ( ret ) {
 		fprintf(stderr, "%s: failed to open filesystem %s\n",
-					argv[0], argv[optind]) ;
-		return 1 ;
+					argv[0], argv[optind]);
+		return 1;
 	}
 
-	pdata.buf = (unsigned char *)malloc(current_fs->blocksize) ;
+	pdata.buf = (unsigned char *)malloc(fs->blocksize);
 	if ( pdata.buf == NULL ) {
-		fprintf(stderr, "%s: out of memory (surely not?)\n", argv[0]) ;
-		return 1 ;
+		fprintf(stderr, "%s: out of memory (surely not?)\n", argv[0]);
+		return 1;
 	}
 
-	ret = ext2fs_read_inode_bitmap(current_fs);
+	ret = ext2fs_read_inode_bitmap(fs);
 	if ( ret ) {
 		fprintf(stderr, "%s: error while reading inode bitmap\n", argv[0]);
-		return 1 ;
+		return 1;
 	}
 
-	ret = ext2fs_read_block_bitmap(current_fs);
+	ret = ext2fs_read_block_bitmap(fs);
 	if ( ret ) {
 		fprintf(stderr, "%s: error while reading block bitmap\n", argv[0]);
-		return 1 ;
+		return 1;
 	}
 
-	root = cwd = EXT2_ROOT_INO ;
+	root = cwd = EXT2_ROOT_INO;
 
 	for ( i=optind+1; i<argc; ++i ) {
-		ret = ext2fs_namei(current_fs, root, cwd, argv[i], &inum) ;
+		ret = ext2fs_namei(fs, root, cwd, argv[i], &inum);
 		if ( ret ) {
-			fprintf(stderr, "%s: failed to find file %s\n", argv[0], argv[i]) ;
-			continue ;
+			fprintf(stderr, "%s: failed to find file %s\n", argv[0], argv[i]);
+			continue;
 		}
 
-		ret = ext2fs_read_inode(current_fs, inum, &inode) ;
+		ret = ext2fs_read_inode(fs, inum, &inode);
 		if ( ret ) {
-			fprintf(stderr, "%s: failed to open inode %d\n", argv[0], inum) ;
-			continue ;
+			fprintf(stderr, "%s: failed to open inode %d\n", argv[0], inum);
+			continue;
 		}
 
 		if ( !ext2fs_inode_has_valid_blocks(&inode) ) {
 			fprintf(stderr, "%s: file %s has no valid blocks\n", argv[0],
-					argv[i]) ;
-			continue ;
+					argv[i]);
+			continue;
 		}
 
 #if defined(EXT4_EXTENTS_FL)
@@ -187,33 +187,33 @@ int main(int argc, char **argv)
 #endif
 
 		if ( verbose ) {
-			printf("processing %s\n", argv[i]) ;
+			printf("processing %s\n", argv[i]);
 		}
 
-		pdata.inode = &inode ;
+		pdata.inode = &inode;
 		pdata.verbose = verbose;
-		pdata.dryrun = dryrun ;
+		pdata.dryrun = dryrun;
 		pdata.count = pdata.blocks = 0;
-		pdata.total_blocks = inode.i_blocks/(current_fs->blocksize >> 9);
+		pdata.total_blocks = inode.i_blocks/(fs->blocksize >> 9);
 		pdata.old_percent = 1000;
-		ret = ext2fs_block_iterate2(current_fs, inum, iter_flags, NULL,
-				process, &pdata) ;
+		ret = ext2fs_block_iterate2(fs, inum, iter_flags, NULL,
+				process, &pdata);
 		if ( ret ) {
 			fprintf(stderr, "%s: failed to process file %s\n", argv[0],
-					argv[i]) ;
-			continue ;
+					argv[i]);
+			continue;
 		}
 
 		if ( pdata.count && !dryrun ) {
-			ret = ext2fs_write_inode(current_fs, inum, &inode) ;
+			ret = ext2fs_write_inode(fs, inum, &inode);
 			if ( ret ) {
 				fprintf(stderr, "%s: failed to write inode data %s\n", argv[0],
-						argv[i]) ;
-				continue ;
+						argv[i]);
+				continue;
 			}
 
-			ext2fs_mark_bb_dirty(current_fs) ;
-			ext2fs_mark_super_dirty(current_fs) ;
+			ext2fs_mark_bb_dirty(fs);
+			ext2fs_mark_super_dirty(fs);
 		}
 
 		if ( verbose ) {
@@ -222,11 +222,11 @@ int main(int argc, char **argv)
 		}
 	}
 
-	ret = ext2fs_close(current_fs) ;
+	ret = ext2fs_close(fs);
 	if ( ret ) {
-		fprintf(stderr, "%s: error while closing filesystem\n", argv[0]) ;
-		return 1 ;
+		fprintf(stderr, "%s: error while closing filesystem\n", argv[0]);
+		return 1;
 	}
 
-	return 0 ;
+	return 0;
 }
--- zerofree.c
+++ zerofree.c
@@ -1,10 +1,10 @@
 /*
  * zerofree - a tool to zero free blocks in an ext2 filesystem
  *
- * Copyright (C) 2004 R M Yorston
+ * Copyright (C) 2004-2012 R M Yorston
  *
  * This file may be redistributed under the terms of the GNU General Public
- * License.
+ * License, version 2.
  *
  * Changes:
  *
@@ -23,160 +23,159 @@
 
 int main(int argc, char **argv)
 {
-	errcode_t ret ;
-	int flags ;
-	int superblock = 0 ;
-	int open_flags = EXT2_FLAG_RW ;
-	int blocksize = 0 ;
-	ext2_filsys current_fs = NULL;
-	unsigned long blk ;
+	errcode_t ret;
+	int flags;
+	int superblock = 0;
+	int open_flags = EXT2_FLAG_RW;
+	int blocksize = 0;
+	ext2_filsys fs = NULL;
+	unsigned long blk;
 	unsigned char *buf;
 	unsigned char *empty;
-	int i, c ;
-	unsigned int free, modified ;
-	double percent ;
-	int old_percent ;
-	unsigned int fillval = 0 ;
-	int verbose = 0 ;
-	int dryrun = 0 ;
+	int i, c;
+	unsigned int free, modified;
+	double percent;
+	int old_percent;
+	unsigned int fillval = 0;
+	int verbose = 0;
+	int dryrun = 0;
 
 	while ( (c=getopt(argc, argv, "nvf:")) != -1 ) {
 		switch (c) {
 		case 'n' :
-			dryrun = 1 ;
-			break ;
+			dryrun = 1;
+			break;
 		case 'v' :
-			verbose = 1 ;
-			break ;
+			verbose = 1;
+			break;
 		case 'f' :
 			{
 				char *endptr;
-				fillval = strtol(optarg, &endptr, 0) ;
+				fillval = strtol(optarg, &endptr, 0);
 				if ( !*optarg || *endptr ) {
-					fprintf(stderr, "%s: invalid argument to -f\n", argv[0]) ;
-					return 1 ;
+					fprintf(stderr, "%s: invalid argument to -f\n", argv[0]);
+					return 1;
 				} else if ( fillval > 0xFFu ) {
-					fprintf(stderr, "%s: fill value must be 0-255\n", argv[0]) ;
-					return 1 ;
+					fprintf(stderr, "%s: fill value must be 0-255\n", argv[0]);
+					return 1;
 				}
 			}
-			break ;
+			break;
 		default :
-			fprintf(stderr, USAGE, argv[0]) ;
-			return 1 ;
+			fprintf(stderr, USAGE, argv[0]);
+			return 1;
 		}
 	}
 
 	if ( argc != optind+1 ) {
-		fprintf(stderr, USAGE, argv[0]) ;
-		return 1 ;
+		fprintf(stderr, USAGE, argv[0]);
+		return 1;
 	}
 
-	ret = ext2fs_check_if_mounted(argv[optind], &flags) ;
+	ret = ext2fs_check_if_mounted(argv[optind], &flags);
 	if ( ret ) {
 		fprintf(stderr, "%s: failed to determine filesystem mount state  %s\n",
-					argv[0], argv[optind]) ;
-		return 1 ;
+					argv[0], argv[optind]);
+		return 1;
 	}
 
 	if ( (flags & EXT2_MF_MOUNTED) && !(flags & EXT2_MF_READONLY) ) {
 		fprintf(stderr, "%s: filesystem %s is mounted rw\n",
-					argv[0], argv[optind]) ;
-		return 1 ;
+					argv[0], argv[optind]);
+		return 1;
 	}
 
 	ret = ext2fs_open(argv[optind], open_flags, superblock, blocksize,
-							unix_io_manager, &current_fs);
+							unix_io_manager, &fs);
 	if ( ret ) {
 		fprintf(stderr, "%s: failed to open filesystem %s\n",
-					argv[0], argv[optind]) ;
-		return 1 ;
+					argv[0], argv[optind]);
+		return 1;
 	}
 
-	empty = (unsigned char *)malloc(current_fs->blocksize) ;
-	buf = (unsigned char *)malloc(current_fs->blocksize) ;
+	empty = (unsigned char *)malloc(fs->blocksize);
+	buf = (unsigned char *)malloc(fs->blocksize);
 
 	if ( empty == NULL || buf == NULL ) {
-		fprintf(stderr, "%s: out of memory (surely not?)\n", argv[0]) ;
-		return 1 ;
+		fprintf(stderr, "%s: out of memory (surely not?)\n", argv[0]);
+		return 1;
 	}
 
-	memset(empty, fillval, current_fs->blocksize);
+	memset(empty, fillval, fs->blocksize);
 
-	ret = ext2fs_read_inode_bitmap(current_fs);
+	ret = ext2fs_read_inode_bitmap(fs);
 	if ( ret ) {
 		fprintf(stderr, "%s: error while reading inode bitmap\n", argv[0]);
-		return 1 ;
+		return 1;
 	}
 
-	ret = ext2fs_read_block_bitmap(current_fs);
+	ret = ext2fs_read_block_bitmap(fs);
 	if ( ret ) {
 		fprintf(stderr, "%s: error while reading block bitmap\n", argv[0]);
-		return 1 ;
+		return 1;
 	}
 
-	free = modified = 0 ;
-	percent = 0.0 ;
-	old_percent = -1 ;
+	free = modified = 0;
+	percent = 0.0;
+	old_percent = -1;
 
 	if ( verbose ) {
-		fprintf(stderr, "\r%4.1f%%", percent) ;
+		fprintf(stderr, "\r%4.1f%%", percent);
 	}
 
-	for ( blk=current_fs->super->s_first_data_block;
-			blk < current_fs->super->s_blocks_count; blk++ ) {
+	for ( blk=fs->super->s_first_data_block;
+			blk < fs->super->s_blocks_count; blk++ ) {
 
-		if ( ext2fs_test_block_bitmap(current_fs->block_map, blk) ) {
-			continue ;
+		if ( ext2fs_test_block_bitmap(fs->block_map, blk) ) {
+			continue;
 		}
 
-		++free ;
+		++free;
 
 		percent = 100.0 * (double)free/
-					(double)current_fs->super->s_free_blocks_count ;
+					(double)fs->super->s_free_blocks_count;
 
 		if ( verbose && (int)(percent*10) != old_percent ) {
-			fprintf(stderr, "\r%4.1f%%", percent) ;
-			old_percent = (int)(percent*10) ;
+			fprintf(stderr, "\r%4.1f%%", percent);
+			old_percent = (int)(percent*10);
 		}
 
-		ret = io_channel_read_blk(current_fs->io, blk, 1, buf);
+		ret = io_channel_read_blk(fs->io, blk, 1, buf);
 		if ( ret ) {
-			fprintf(stderr, "%s: error while reading block\n", argv[0]) ;
-			return 1 ;
+			fprintf(stderr, "%s: error while reading block\n", argv[0]);
+			return 1;
 		}
 
-		for ( i=0; i < current_fs->blocksize; ++i ) {
+		for ( i=0; i < fs->blocksize; ++i ) {
 			if ( buf[i] != fillval ) {
-				break ;
+				break;
 			}
 		}
 
-		if ( i == current_fs->blocksize ) {
-			continue ;
+		if ( i == fs->blocksize ) {
+			continue;
 		}
 
-		++modified ;
+		++modified;
 
 		if ( !dryrun ) {
-			ret = io_channel_write_blk(current_fs->io, blk, 1, empty) ;
+			ret = io_channel_write_blk(fs->io, blk, 1, empty);
 			if ( ret ) {
-				fprintf(stderr, "%s: error while writing block\n", argv[0]) ;
-				return 1 ;
+				fprintf(stderr, "%s: error while writing block\n", argv[0]);
+				return 1;
 			}
 		}
 	}
 
 	if ( verbose ) {
-		printf("\r%u/%u/%u\n", modified, free,
-				current_fs->super->s_blocks_count) ;
+		printf("\r%u/%u/%u\n", modified, free, fs->super->s_blocks_count);
 	}
 
-	ret = ext2fs_close(current_fs) ;
+	ret = ext2fs_close(fs);
 	if ( ret ) {
-		fprintf(stderr, "%s: error while closing filesystem\n", argv[0]) ;
-		return 1 ;
+		fprintf(stderr, "%s: error while closing filesystem\n", argv[0]);
+		return 1;
 	}
 
-	return 0 ;
+	return 0;
 }
